1890fc585b0403d824f6129ad7f28b96d41c4c00
Antho <transkuja@hotmail.fr>
2020-11-02 17:08:11 +0100

    Billboard damage feedback

---
+++ Assets/Scripts/UI/Billboard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        transform.rotation = mainCamera.transform.rotation;
+    }
+}
