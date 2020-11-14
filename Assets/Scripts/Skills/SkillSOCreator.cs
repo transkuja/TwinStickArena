3fb844b3ecf606d288e6820f5db6accd0bacf046
Antho <transkuja@hotmail.fr>
2020-11-14 15:14:24 +0100

    Start working on skills

---
+++ Assets/Scripts/Skills/SkillSOCreator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SkillSOCreator
+{
+    [MenuItem("Assets/Create/Skill Object")]
+    public static void CreateSkillSO()
+    {
+        SkillSO asset = ScriptableObject.CreateInstance<SkillSO>();
+
+        AssetDatabase.CreateAsset(asset, "Assets/ScriptableObjects/Waves/NewSkillSO.asset");
+        AssetDatabase.SaveAssets();
+
+        EditorUtility.FocusProjectWindow();
+
+        Selection.activeObject = asset;
+    }
+}
